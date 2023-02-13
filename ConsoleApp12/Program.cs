0a9c03ef405c9ecfdae79baf4b2920a3103895e0
Rafał Staniewski <Rafal6121@gmail.com>
2023-02-13 19:15:25 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //var builder = WebApplication.CreateBuilder(args);
+            //var movieApiKey = builder.Configuration["Movies:ServiceApiKey"];
+        }
+    }
+}
